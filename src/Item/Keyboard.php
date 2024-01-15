1bf6b7393ad6797cb0f0132a1b21a76bb24c7af0
Norbert Kecső <norbert.kecso@topin.hu>
2024-01-15 12:00:37 +0100

    typehint

--- src/Item/Keyboard.php
+++ src/Item/Keyboard.php
@@ -2,11 +2,18 @@
 
 namespace Webdream\Warehouse\Item;
 
+use Webdream\Warehouse\Core\Brand;
 use Webdream\Warehouse\Core\Product;
 
 class Keyboard extends Product
 {
-    public function __construct($sku, $name, $price, $brand, private readonly bool $isMechanical)
+    public function __construct(
+        private readonly string $sku,
+        private readonly string $name,
+        private readonly int $price,
+        private readonly Brand $brand,
+        private readonly bool $isMechanical
+    )
     {
         parent::__construct($sku, $name, $price, $brand);
     }
--- src/Item/Mouse.php
+++ src/Item/Mouse.php
@@ -2,11 +2,18 @@
 
 namespace Webdream\Warehouse\Item;
 
+use Webdream\Warehouse\Core\Brand;
 use Webdream\Warehouse\Core\Product;
 
 class Mouse extends Product
 {
-    public function __construct($sku, $name, $price, $brand, private readonly int $dpi)
+    public function __construct(
+        private readonly string $sku,
+        private readonly string $name,
+        private readonly int $price,
+        private readonly Brand $brand,
+        private readonly int $dpi
+    )
     {
         parent::__construct($sku, $name, $price, $brand);
     }
